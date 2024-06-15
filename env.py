896dea8cdc497b9d5e372ce8214060f7dcae788d
Anass Muhammad Ali <syriacano@gmail.com>
2024-06-15 00:59:29 +0000

    :intial commit

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['DATABASE_URL'] = 'postgres://mgdntzvk:MwKPVb1OgvTWKO3IcBc24clyHYV7CFxo@flora.db.elephantsql.com/mgdntzvk'
+os.environ['SECRET_KEY'] = '$89u5o*u^6^8=^_pjb5@#@28_+svh%*^$84-ub-1#k@x_4gz%f'
